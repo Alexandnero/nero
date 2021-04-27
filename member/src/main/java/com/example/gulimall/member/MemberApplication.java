852aade330df67c894fd7fbc3e96ae8af4c8ce74
AlexNero <455604918@qq.com>
2021-04-27 09:11:36 +0800

    2021.4

--- member/src/main/java/com/example/gulimall/member/MemberApplication.java
+++ member/src/main/java/com/example/gulimall/member/MemberApplication.java
@@ -2,8 +2,13 @@ package com.example.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.gulimall.member")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.example.gulimall.member.feign")
 public class MemberApplication {
 
     public static void main(String[] args) {
